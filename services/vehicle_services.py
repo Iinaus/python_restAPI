80b7b135b72f4d134a33331453821ea5152f87f0
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-11-11 10:55:51 +0200

    Add mysql repositories

--- services/vehicle_services.py
+++ services/vehicle_services.py
@@ -4,7 +4,7 @@ import models
 class VehicleService:
     @staticmethod
     def vehicle_tuple_to_vehicle(vehicle_tuple):
-        return models.vehicle(
+        return models.Vehicle(
             _id = vehicle_tuple[0],
             make = vehicle_tuple[1],
             model = vehicle_tuple[2]
