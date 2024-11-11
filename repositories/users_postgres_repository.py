06af770284b9de4f77072ebdc95831c60d4e5891
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-11-11 11:12:20 +0200

    Add postgres repositories methods

--- repositories/users_postgres_repository.py
+++ repositories/users_postgres_repository.py
@@ -1,18 +1,74 @@
+import models
+from werkzeug.exceptions import NotFound
+
+from services.user_services import UserService
+
 class UsersPostgresRepository:
     def __init__(self, con):
         self.con = con
         
     def get_all(self):
-        pass
+        with self.con.cursor() as cur:
+            cur.execute('SELECT * FROM users')
+            result = cur.fetchall()
+            users = []
+            for user in result:
+                users.append(models.User(user[0], user[1], user[2], user[3]))
+            return users
+        
+    def get_by_id(self, id):
+        with self.con.cursor() as cur:
+            cur.execute("SELECT * FROM users WHERE id = %s", (id,))
+            user_tuple = cur.fetchone()
+            if user_tuple is None:
+                raise NotFound('user not found')
+            user = UserService.user_tuple_to_user(user_tuple)
+            return user
 
-    def get_by_id(self):
-        pass
+    def create(self, user):
+        try:
+            with self.con.cursor() as cur:
+                sql = """
+                        INSERT INTO users (username, firstname, lastname)
+                        VALUES (%s, %s, %s)
+                        RETURNING id
+                    """
+                values = (user.username, user.firstname, user.lastname)
+                cur.execute(sql, values)
+                user_id = cur.fetchone()[0]
+                self.con.commit()
+                user = models.User(user_id, user.username, user.firstname, user.lastname)
+                return user
+        except Exception as e:
+            self.con.rollback()
+            raise e
 
-    def create(self):
-        pass
+    def update_by_id(self, user, req_data):
+        try:
+            user.username = req_data['username']
+            user.firstname = req_data['firstname']
+            user.lastname = req_data['lastname']
+            
+            with self.con.cursor() as cur:
+                sql = """
+                        UPDATE users 
+                        SET username = %s, firstname = %s, lastname = %s 
+                        WHERE id = %s
+                        """
+                values = (user.username, user.firstname, user.lastname, user.id)
+                cur.execute(sql, values)
+                self.con.commit()
+                user = models.User(user.id, user.username, user.firstname, user.lastname)
+                return user
+        except Exception as e:
+            self.con.rollback()
+            raise e
 
-    def update_by_id(self):
-        pass
-
-    def delete_by_id(self):
-        pass
+    def delete_by_id(self, user):
+        try:
+            with self.con.cursor() as cur:
+                cur.execute("DELETE FROM users WHERE id = %s", (user.id,))
+                self.con.commit()
+        except Exception as e:
+            self.con.rollback()
+            raise e
--- repositories/vehicles_postgres_repository.py
+++ repositories/vehicles_postgres_repository.py
@@ -1,18 +1,74 @@
+import models
+from werkzeug.exceptions import NotFound
+
+from services.vehicle_services import VehicleService
+
 class VehiclesPostgresRepository:
     def __init__(self, con):
         self.con = con
         
     def get_all(self):
-        pass
+        with self.con.cursor() as cur:
+            cur.execute('SELECT * FROM vehicles')
+            result = cur.fetchall()
+            vehicles = []
+            for vehicle in result:
+                vehicles.append(models.Vehicle(vehicle[0], vehicle[1], vehicle[2]))
+            return vehicles
+        
+    def get_by_id(self, id):
+        with self.con.cursor() as cur:
+            cur.execute("SELECT * FROM vehicles WHERE id = %s", (id,))
+            vehicle_tuple = cur.fetchone()
+            if vehicle_tuple is None:
+                raise NotFound('vehicle not found')
+            vehicle = VehicleService.vehicle_tuple_to_vehicle(vehicle_tuple)
+            return vehicle
 
-    def get_by_id(self):
-        pass
+    def create(self, vehicle):
+        try:
+            with self.con.cursor() as cur:
+                sql = """
+                        INSERT INTO vehicles (make, model)
+                        VALUES (%s, %s)
+                        RETURNING id
+                    """
+                values = (vehicle.make, vehicle.model)
+                cur.execute(sql, values)
+                vehicle_id = cur.fetchone()[0]
+                self.con.commit()
+                vehicle = models.Vehicle(vehicle_id, vehicle.make, vehicle.model)
+                return vehicle
+        except Exception as e:
+            self.con.rollback()
+            raise e
 
-    def create(self):
-        pass
 
-    def update_by_id(self):
-        pass
+    def update_by_id(self, vehicle, req_data):
+        try:
+            vehicle.make = req_data['make']
+            vehicle.model = req_data['model']
+            
+            with self.con.cursor() as cur:
+                sql = """
+                        UPDATE vehicles 
+                        SET make = %s, model = %s
+                        WHERE id = %s
+                        """
+                values = (vehicle.make, vehicle.model, vehicle.id)
+                cur.execute(sql, values)
+                self.con.commit()
+                vehicle = models.Vehicle(vehicle.id, vehicle.make, vehicle.model)
+                return vehicle
+        except Exception as e:
+            self.con.rollback()
+            raise e
 
-    def delete_by_id(self):
-        pass
+    def delete_by_id(self, vehicle):
+        try:
+            with self.con.cursor() as cur:
+                cur.execute("DELETE FROM vehicles WHERE id = %s", (vehicle.id,))
+                self.con.commit()
+        except Exception as e:
+            self.con.rollback()
+            raise e
