6d6cce84751b067c1cb9426d806e7a2f7203dd20
Iina Soikkeli <119871761+Iinaus@users.noreply.github.com>
2024-11-11 10:29:40 +0200

    Add vehicles_controller methods and vehicles_mongodb_repository

--- controllers/vehicles_controller.py
+++ controllers/vehicles_controller.py
@@ -1,13 +1,18 @@
-from flask import jsonify
+from flask import jsonify, request
+from werkzeug.exceptions import NotFound
 
 from decorators.db_connection_decorator import get_db_connection
 from decorators.repository_decorator import get_repository
+import models
+import services.vehicle_services
 
 @get_db_connection
 @get_repository('vehicles')
 def get_all_vehicles(repo):
     try:
-        repo.get_all()
+        vehicles = repo.get_all()
+        vehicles_json = services.vehicle_services.VehicleService.list_to_json(vehicles)
+        return jsonify(vehicles_json)
     except Exception as e:
         return jsonify({'err': str(e)}), 500
 
@@ -15,7 +20,10 @@ def get_all_vehicles(repo):
 @get_repository('vehicles')
 def get_vehicle_by_id(repo, id):
     try:
-        repo.get_by_id()
+        vehicle = repo.get_by_id(id)
+        return jsonify(vehicle.to_json())
+    except NotFound:
+        return jsonify({'err': 'vehicle not found'}), 404
     except Exception as e:
         return jsonify({'err': str(e)}), 500
 
@@ -23,7 +31,10 @@ def get_vehicle_by_id(repo, id):
 @get_repository('vehicles')
 def create_vehicle(repo):
     try:
-        repo.create()
+        req_data = request.get_json()
+        new_vehicle = models.Vehicle(0, req_data['make'], req_data['model'])
+        vehicle = repo.create(new_vehicle)
+        return jsonify(vehicle.to_json())
     except Exception as e:
         return jsonify({'err': str(e)}), 500
 
@@ -31,7 +42,12 @@ def create_vehicle(repo):
 @get_repository('vehicles')
 def update_vehicle_by_id(repo, id):
     try:
-        repo.update_by_id()
+        vehicle = repo.get_by_id(id)
+        req_data = request.get_json()
+        updated_vehicle = repo.update_by_id(vehicle, req_data)
+        return jsonify(updated_vehicle.to_json())
+    except NotFound:
+        return jsonify({'err': 'vehicle not found'}), 404
     except Exception as e:
         return jsonify({'err': str(e)}), 500
 
@@ -39,6 +55,10 @@ def update_vehicle_by_id(repo, id):
 @get_repository('vehicles')
 def delete_vehicle_by_id(repo, id):
     try:
-        repo.delete_by_id()
+        vehicle = repo.get_by_id(id)
+        repo.delete_by_id(vehicle)
+        return jsonify(), 200
+    except NotFound:
+        return jsonify({'err': 'vehicle not found'}), 404
     except Exception as e:
         return jsonify({'err': str(e)}), 500
--- repositories/vehicles_mongodb_repository.py
+++ repositories/vehicles_mongodb_repository.py
@@ -1,18 +1,51 @@
+from bson import ObjectId
+import models
+from werkzeug.exceptions import NotFound
+
 class VehiclesMongodbRepository:
     def __init__(self, con):
         self.con = con
+        self.vehicles_collection = self.con['vehicles']
         
     def get_all(self):
-        pass
+        result = self.vehicles_collection.find()
+        vehicles = []
+        for vehicle in result:
+            vehicles.append(models.Vehicle(str(vehicle['_id']), vehicle['make'], vehicle['model']))
+        return vehicles
+        
+    def get_by_id(self, id):
+        result = self.vehicles_collection.find_one({"_id": ObjectId(id)})
+        if result is None:
+            raise NotFound('vehicle not found')
+        vehicle = models.Vehicle(str(result['_id']), result['make'], result['model'])
+        return vehicle
 
-    def get_by_id(self):
-        pass
+    def create(self, vehicle):
+        try:
+            result = self.vehicles_collection.insert_one({"make": vehicle.make, "model": vehicle.model})
+            vehicle = models.Vehicle(str(result.inserted_id), vehicle.make, vehicle.model)
+            return vehicle
+        except Exception as e:
+            raise e
 
-    def create(self):
-        pass
+    def update_by_id(self, vehicle, req_data):
+        try:
+            vehicle.make = req_data['make']
+            vehicle.model = req_data['model']
+            
+            self.vehicles_collection.update_one(
+                {"_id": ObjectId(vehicle.id)},
+                {"$set": {"make": vehicle.make, "model": vehicle.model}}
+            )
 
-    def update_by_id(self):
-        pass
+            vehicle = models.Vehicle(vehicle.id, vehicle.make, vehicle.model)
+            return vehicle
+        except Exception as e:
+            raise e
 
-    def delete_by_id(self):
-        pass
+    def delete_by_id(self, vehicle):
+        try:
+            self.vehicles_collection.delete_one({"_id": ObjectId(vehicle.id)})
+        except Exception as e:
+            raise e
